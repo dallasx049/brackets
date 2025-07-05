4d9393f7a55b5c914f6b0e0fb5b32be8bcfec0f5
Vladyslav <dallasx049@gmail.com>
2025-07-05 12:17:09 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const bracketPairs = bracketsConfig.reduce((acc, [opening, closing]) => {
+    acc[opening] = closing;
+    return acc;
+  }, {});
+
+  const openingBrackets = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    const curr = str[i];
+    const top = openingBrackets.at(-1);
+
+    // Opening bracket branch
+    if (bracketPairs[curr]) {
+      // Only distinct pairs
+      if (bracketPairs[curr] !== curr) {
+        openingBrackets.push(curr);
+      }
+      // Opening and closing brackets are the same
+      else if (top !== curr) {
+        openingBrackets.push(curr);
+      } else {
+        openingBrackets.pop();
+      }
+    }
+    // Closing bracket branch
+    else if (bracketPairs[top] === curr) {
+      openingBrackets.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return !openingBrackets.length;
 };
